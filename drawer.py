fc63a7ced402704b0fefcdcf7fcfd6238d897aac
knn1212 <nguyenvietkhanh12c7@gmail.com>
2024-04-02 15:53:49 +0700

    make some change

--- drawer.py
+++ drawer.py
@@ -1,10 +1,14 @@
 import pygame
 
 class Maze(Node):
-    def __init(self, surface: pygame.surface.Surface) -> None:
+    def __init(
         self,
         value: str
         state: tuple[int, int],
         cost: int,
-        
+        parent: Node | None = None,
+        action: str | None = None,
+        color: tuple[int, int, int] = WHITE
+        ) -> None:
+
 
